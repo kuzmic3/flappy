d041f15dd3a7574e81a490e1e03554c882f0ff62
Uros Djordjevic <uros.djordjevic3009@gmail.com>
2023-06-16 16:47:56 +0200

    fix create account form data

--- create-account.php
+++ create-account.php
@@ -8,7 +8,6 @@ curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $_POST);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_setopt($curl, CURLOPT_USERPWD, "api_flappy:^6gGygf65I");
 
 $response = curl_exec($curl);
